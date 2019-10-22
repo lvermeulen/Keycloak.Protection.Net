f1a0f3331d6513cdfb60866fed01bd0a7bc597e0
Luk Vermeulen <luk.vermeulen@gmail.com>
2019-10-22 10:10:11 +0200

    refine error handling

--- src/Keycloak.Protection.Net/Common/HttpResponseMessageExtensions.cs
+++ src/Keycloak.Protection.Net/Common/HttpResponseMessageExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -7,10 +8,12 @@ namespace Keycloak.Protection.Net.Common
 {
     public static class HttpResponseMessageExtensions
     {
+        private static bool IsInRange(this HttpStatusCode statusCode, int lowerInclusive, int upperExclusive) => (int)statusCode >= lowerInclusive && (int)statusCode < upperExclusive;
+
         public static async Task<HttpResponseMessage> HandleErrorsAsync(this Task<HttpResponseMessage> responseMessageTask)
         {
             var response = await responseMessageTask;
-            if (!response.IsSuccessStatusCode)
+            if (!response.IsSuccessStatusCode && response.StatusCode.IsInRange(400, 500))
             {
                 string content = await response.Content.ReadAsStringAsync();
                 var error = JsonConvert.DeserializeObject<ErrorEntity>(content);
